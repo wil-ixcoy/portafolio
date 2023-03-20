18f49e3de483e9dc7f7a286ef5871c6311dd3cc7
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2023-03-20 12:24:07 -0600

    diseño de componente about

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,20 +1,25 @@
 <template>
-  <div>
-    <h1 class="text-5xl mt-7">Sobre mí</h1>
-    <div class="grid grid-cols-2 gap-4 mt-4">
-      <section>
-        <img src="../assets/vue.svg" alt="" />
-      </section>
+  <div class="bg-secundary">
+    <section class="grid grid-cols-2 gap-2 mt-4 py-5 px-24">
+        <img src="../assets/fotoperfil.png" alt="" />
 
       <section class="">
-        <p class="text-left">
+        <h1 class=" text-left text-5xl mt-7 text-colorwhite mb-4">Sobre <i class="text-primary">mí</i></h1>
+
+        <p class="text-left text-colorwhite">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. A error,
+          libero sapiente distinctio sunt neque dignissimos possimus eius
+          voluptatum nobis officia autem itaque culpa quam quidem illum eos quis
+          molestias.
+        </p>
+        <p class="text-left text-colorwhite">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. A error,
           libero sapiente distinctio sunt neque dignissimos possimus eius
           voluptatum nobis officia autem itaque culpa quam quidem illum eos quis
           molestias.
         </p>
       </section>
-    </div>
+    </section>
   </div>
 </template>
 
