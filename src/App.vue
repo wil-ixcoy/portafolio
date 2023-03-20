98ce81e10141b9d6eb0a9cd00c573bab1a37458e
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2023-03-19 22:02:21 -0600

    se crea componente about

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from './components/header.vue'
+import About from './components/about.vue'
 </script>
 
 <template>
 <Header />
+<About />
 </template>
 
 <style scoped>
---
+++ src/components/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 class="text-5xl mt-7">Sobre mí</h1>
+    <div class="grid grid-cols-2 gap-4 mt-4">
+      <section>
+        <img src="../assets/vue.svg" alt="" />
+      </section>
+
+      <section class="">
+        <p class="text-left">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. A error,
+          libero sapiente distinctio sunt neque dignissimos possimus eius
+          voluptatum nobis officia autem itaque culpa quam quidem illum eos quis
+          molestias.
+        </p>
+      </section>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+img {
+  width: 400px;
+  height: 400px;
+}
+</style>
