1c374c6fdc757d224f61fc9757a7547d935514ae
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2023-03-19 21:24:54 -0600

    inicio de mi portafolio profesional

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Header from './components/header.vue'
+</script>
+
+<template>
+<Header />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+  <section class="grid grid-cols-2 gap-10 mx-auto">
+    <article class="p-20">
+      <h1 class="text-left text-5xl">Hola, soy Wiliams</h1>
+      <p class="text-left text-2xl pt-3">Backend y frontend developer</p>
+      <button class="rounded-md border-black 4y-3 px-2">Contactame</button>
+    </article>
+    <article class="">
+        <img src="../assets/vue.svg" alt="">
+    </article>
+  </section>
+  
+</template>
+
+<style scoped>
+img{
+    width: 400px;
+    height: 400px;
+}
+</style>
