8186eb3d1481ea0de7b60fe41483d04b2044d4b9
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2023-03-20 12:10:30 -0600

    se agregan redes sociales

--- src/components/header.vue
+++ src/components/header.vue
@@ -5,19 +5,44 @@
   <section class="grid grid-cols-2 gap-10 mx-auto">
     <article class="p-20">
       <h1 class="text-left text-colorwhite text-5xl">Hola, soy Wiliams</h1>
-      <p class="text-left text-colorwhite text-2xl pt-3">Backend y frontend developer</p>
-      <button class="rounded-md border-black py-2  px-5 my-4 bg-primary ">Contactame</button>
+      <p class="text-left text-colorwhite text-2xl pt-3">
+        Backend y frontend developer
+      </p>
+
+      <section class="grid grid-cols-4 gap-3 my-4">
+        <a target="_blank" href="https://github.com/wil-ixcoy" class="text-2xl text-primary">
+          <i class="fab fa-github"></i>
+        </a>
+        <a
+         target="_blank" href="https://www.linkedin.com/in/wiliamsixcoy/"
+          class="text-2xl text-primary"
+        >
+          <i class="fab fa-linkedin"></i>
+        </a>
+        <a
+         target="_blank" href="https://www.instagram.com/wil_ixcoy/"
+          class="text-2xl text-primary"
+        >
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a target="_blank" href="https://twitter.com/wil_ixcoy" class="text-2xl text-primary">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </section>
+
+      <button class="rounded-lg border-black py-2 px-5 my-4 bg-primary">
+        Contactame
+      </button>
     </article>
     <article class="">
-        <img src="../assets/vue.svg" alt="">
+      <img src="../assets/fotoperfil.png" alt="" />
     </article>
   </section>
-  
 </template>
 
 <style scoped>
-img{
-    width: 400px;
-    height: 400px;
+img {
+  width: 400px;
+  height: 400px;
 }
 </style>
