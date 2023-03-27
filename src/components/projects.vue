c11a21741ae22eb91f28674afd52595d263e042d
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2023-03-27 15:33:13 -0600

    se agregan los otros proyectos con su descripcion y enlaces para ver el demo y el repositorio en github

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -145,7 +145,68 @@
   <h2 class="text-3xl text-left text-primary mt-12">Proyectos Backend:</h2>
 
   <section class="grid grid-cols-4 gap-4">
-    <!-- primer proyecto Draux API -->
+    <!-- primer proyecto clon de pinterest-->
+    <section class="bg-secundary rounded-md drop-shadow-2xl mt-6">
+      <img
+        src="https://picsum.photos/200/300"
+        alt=""
+        class="w-full h-28 rounded-t-md"
+      />
+      <div class="p-3">
+        <h3 class="text-primary text-xl text-left">Clon de Pinterest</h3>
+        <p class="text-colorwhite text-left">
+          Es una app realizada con el fin de poder mejorar mis habilidades de
+          programación del lado del backend. Uso de multer para la subida de
+          imagénes, cloudinary para almacenamiento.
+        </p>
+
+        <section class="grid grid-cols-5 gap-3 my-4 px-3">
+          <img
+            class="w-7 h-7"
+            src="../assets/tecnologias/nodejs-icon.svg"
+            alt="nodejs"
+          />
+          <img
+            class="w-7 h-7"
+            src="../assets/tecnologias/sequelize.svg"
+            alt=""
+          />
+          <img
+            class="w-7 h-7"
+            src="../assets/tecnologias/postgresql.svg"
+            alt="postgresql"
+          />
+          <img
+            class="w-7 h-7"
+            src="../assets/tecnologias/javascript.svg"
+            alt="js"
+          />
+
+          <img
+            class="w-7 h-7"
+            src="../assets/tecnologias/docker-icon.svg"
+            alt="docker"
+          />
+        </section>
+
+        <div class="flex justify-between mt-5 px-8">
+          <button
+            class="bg-primary rounded py-2 px-4"
+            onclick="window.open('https://github.com/wil-ixcoy/Pinterest_Clon')"
+          >
+            GitHub
+          </button>
+          <button
+            class="text-colorwhite border-primary rounded py-2 px-4"
+            onclick="window.open('https://pinterestclon-production.up.railway.app/api/docs/')"
+          >
+            Demo
+          </button>
+        </div>
+      </div>
+    </section>
+
+    <!-- segundo proyecto Draux API-->
     <section class="bg-secundary rounded-md drop-shadow-2xl mt-6">
       <img
         src="https://picsum.photos/200/300"
@@ -155,9 +216,9 @@
       <div class="p-3">
         <h3 class="text-primary text-xl text-left">DRAUX API</h3>
         <p class="text-colorwhite text-left">
-          Weather app es una web interactiva que muestra datos meteorológicos de
-          distintas ciudades del mundo. Solo tienes que buscar la ciudad que
-          desees.
+          Draux es una plataforma web destinada a conectar a profesionales y
+          aficionados del diseño UX. Ofrece un espacio para que los usuarios
+          compartan contenido relacionado con el diseño UX.
         </p>
 
         <section class="grid grid-cols-5 gap-3 my-4 px-3">
@@ -192,21 +253,20 @@
         <div class="flex justify-between mt-5 px-8">
           <button
             class="bg-primary rounded py-2 px-4"
-            onclick="window.open('https://github.com/wil-ixcoy/DraUX_API')"
+            onclick="window.open('https://github.com/wil-ixcoy/Draux')"
           >
             GitHub
           </button>
           <button
             class="text-colorwhite border-primary rounded py-2 px-4"
-            onclick="window.open('https://jolly-dubinsky-4b78aa.netlify.app/#/')"
+            onclick="window.open('https://draux-production.up.railway.app/api/v1/docs/')"
           >
             Demo
           </button>
         </div>
       </div>
     </section>
-
-      <!-- segundo proyecto Pinterest-->
+    <!-- tercer proyecto Playit-->
     <section class="bg-secundary rounded-md drop-shadow-2xl mt-6">
       <img
         src="https://picsum.photos/200/300"
@@ -214,11 +274,11 @@
         class="w-full h-28 rounded-t-md"
       />
       <div class="p-3">
-        <h3 class="text-primary text-xl text-left">DRAUX API</h3>
+        <h3 class="text-primary text-xl text-left">Platzi Store</h3>
         <p class="text-colorwhite text-left">
-          Weather app es una web interactiva que muestra datos meteorológicos de
-          distintas ciudades del mundo. Solo tienes que buscar la ciudad que
-          desees.
+          latzi Store es una aplicación desarrollada con Node.js para construir
+          una tienda de venta de productos. A continuación detallo la ruta de
+          cursos que hemos seguido para desarrollar esta aplicación.
         </p>
 
         <section class="grid grid-cols-5 gap-3 my-4 px-3">
@@ -250,84 +310,18 @@
           />
         </section>
 
-        <div class="flex justify-between mt-5 px-8">
+        <div class=" mt-5 px-8">
           <button
             class="bg-primary rounded py-2 px-4"
-            onclick="window.open('https://github.com/wil-ixcoy/Clon_Pinterest')"
+            onclick="window.open('https://github.com/wil-ixcoy/myStore-nodeExpress')"
           >
             GitHub
-          </button>
-          <button
-            class="text-colorwhite border-primary rounded py-2 px-4"
-            onclick="window.open('https://jolly-dubinsky-4b78aa.netlify.app/#/')"
-          >
-            Demo
-          </button>
-        </div>
-      </div>
-    </section>
-          <!-- tercer proyecto Playit-->
-    <section class="bg-secundary rounded-md drop-shadow-2xl mt-6">
-      <img
-        src="https://picsum.photos/200/300"
-        alt=""
-        class="w-full h-28 rounded-t-md"
-      />
-      <div class="p-3">
-        <h3 class="text-primary text-xl text-left">PlayIT</h3>
-        <p class="text-colorwhite text-left">
-          Weather app es una web interactiva que muestra datos meteorológicos de
-          distintas ciudades del mundo. Solo tienes que buscar la ciudad que
-          desees.
-        </p>
-
-        <section class="grid grid-cols-5 gap-3 my-4 px-3">
-          <img
-            class="w-7 h-7"
-            src="../assets/tecnologias/nodejs-icon.svg"
-            alt="nodejs"
-          />
-          <img
-            class="w-7 h-7"
-            src="../assets/tecnologias/sequelize.svg"
-            alt=""
-          />
-          <img
-            class="w-7 h-7"
-            src="../assets/tecnologias/postgresql.svg"
-            alt="postgresql"
-          />
-          <img
-            class="w-7 h-7"
-            src="../assets/tecnologias/javascript.svg"
-            alt="js"
-          />
-
-          <img
-            class="w-7 h-7"
-            src="../assets/tecnologias/docker-icon.svg"
-            alt="docker"
-          />
-        </section>
-
-        <div class="flex justify-between mt-5 px-8">
-          <button
-            class="bg-primary rounded py-2 px-4"
-            onclick="window.open('https://github.com/wil-ixcoy/DraUX_API')"
-          >
-            GitHub
-          </button>
-          <button
-            class="text-colorwhite border-primary rounded py-2 px-4"
-            onclick="window.open('https://jolly-dubinsky-4b78aa.netlify.app/#/')"
-          >
-            Demo
           </button>
         </div>
       </div>
     </section>
 
-          <!-- primer proyecto Telegram Bot-->
+    <!-- primer proyecto Telegram Bot-->
     <section class="bg-secundary rounded-md drop-shadow-2xl mt-6">
       <img
         src="https://picsum.photos/200/300"
@@ -337,12 +331,12 @@
       <div class="p-3">
         <h3 class="text-primary text-xl text-left">Bot de Telegram</h3>
         <p class="text-colorwhite text-left">
-          Weather app es una web interactiva que muestra datos meteorológicos de
-          distintas ciudades del mundo. Solo tienes que buscar la ciudad que
-          desees.
+          Bot de telegram, cuya función principal es poder mostrar en tiempo
+          real el clima de las distintas ciudades del mundo, solo tienes que
+          escribir el nombre de la ciudad que deseas consultar.
         </p>
 
-        <section class="grid grid-cols-5 gap-3 my-4 px-3">
+        <section class="grid grid-cols-3 gap-3 my-4 px-3">
           <img
             class="w-7 h-7"
             src="../assets/tecnologias/nodejs-icon.svg"
@@ -350,24 +344,13 @@
           />
           <img
             class="w-7 h-7"
-            src="../assets/tecnologias/sequelize.svg"
-            alt=""
-          />
-          <img
-            class="w-7 h-7"
-            src="../assets/tecnologias/postgresql.svg"
-            alt="postgresql"
-          />
-          <img
-            class="w-7 h-7"
             src="../assets/tecnologias/javascript.svg"
             alt="js"
           />
-
           <img
             class="w-7 h-7"
-            src="../assets/tecnologias/docker-icon.svg"
-            alt="docker"
+            src="../assets/tecnologias/axios.svg"
+            alt="axios"
           />
         </section>
 
@@ -380,7 +363,7 @@
           </button>
           <button
             class="text-colorwhite border-primary rounded py-2 px-4"
-            onclick="window.open('https://t.me/wiliamsTI_bot')"
+            onclick="window.open('https://t.me/weatherByWiliams_bot')"
           >
             Demo
           </button>
